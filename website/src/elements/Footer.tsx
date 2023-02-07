afdbd7552524a716a53432ab909278ddf77d90d9
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-02-07 17:22:07 +0000

    refactor(hrefs): add-new-domain

--- website/src/elements/Footer.tsx
+++ website/src/elements/Footer.tsx
@@ -3,9 +3,9 @@
 function Footer() {
     return (
         <section className='px-8 pt-12 pb-6'>
-            <div className='sm:w-[68rem] pt-4 sm:mx-auto flex flex-wrap justify-between text-gray-700 dark:text-zinc-400 border-t border-gray-200 dark:border-zinc-700'>
-                <small>Made with ☕️ by <a className='hover:underline' href='https://github.com/abiddiscombe'>abiddiscombe</a></small>
-                <small><span className='text-gray-500'><a href='https://archiebiddiscombe.com'>archiebiddiscombe.com</a></span></small>
+            <div className='sm:w-[68rem] pt-4 sm:mx-auto flex flex-wrap justify-between text-gray-400 dark:text-zinc-600 border-t border-gray-200 dark:border-zinc-700'>
+                <small>Made with ☕️ by <a className='hover:underline' href='https://github.com/abiddiscombe'>@abiddiscombe</a></small>
+                <small><span className='text-gray-400 dark:text-zinc-600'><a href='https://abiddiscombe.dev'>abiddiscombe.dev</a></span></small>
             </div>
 
         </section>
--- website/src/elements/Header.tsx
+++ website/src/elements/Header.tsx
@@ -3,8 +3,8 @@
 function Header () {
 	return (
 		<header className='p-8 flex flex-wrap justify-between items-center'>
-			<h1 className='text-lg text-gray-700 dark:text-zinc-200 font-semibold hidden sm:block'>
-				<a className='hover:underline' href='https://arch.codes'>arch.codes</a>
+			<h1 className='text-lg text-gray-400 dark:text-zinc-600 font-semibold hidden sm:block'>
+				<a className='hover:underline' href='https://abiddiscombe.dev'>abiddiscombe.dev</a>
 			</h1>
 			<nav className='flex gap-4 text-gray-700  dark:text-zinc-200'>
 				<a className='hover:underline' href='https://github.com/abiddiscombe'>GitHub</a>
