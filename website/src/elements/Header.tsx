2ed063cc0bbd50701ce36080cabf6a071f83bb7e
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-02-15 19:46:52 +0000

    feat: add-header-bg

--- website/src/elements/Header.tsx
+++ website/src/elements/Header.tsx
@@ -2,10 +2,10 @@
 
 export default () => {
 	return (
-		<header>
-			<div className='p-8 items-center flex gap-4'>
-				<h1 className='flex-grow text-lg text-gray-400 dark:text-zinc-600 font-semibold hidden sm:block'>
-					abiddiscombe.dev
+		<header className='bg-white dark:bg-zinc-800 border-b border-gray-200 dark:border-zinc-700'>
+			<div className='px-8 py-6 items-center flex gap-4'>
+				<h1 className='flex-grow text-lg text-gray-400 dark:text-zinc-600 hidden sm:block'>
+					@<b>abiddiscombe</b>
 				</h1>
 				<a className='text-gray-700 dark:text-zinc-200 hover:underline' href='https://github.com/abiddiscombe'>GitHub</a>
 				<a className='text-gray-700 dark:text-zinc-200 hover:underline' href='https://linkedin.com/in/archiebiddiscombe'>LinkedIn</a>
