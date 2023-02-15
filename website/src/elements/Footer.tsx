38b117d08acc779da89b7e48d7d2cfb4e174db63
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-02-15 20:00:27 +0000

    chore: change-footer-text

--- website/src/elements/Footer.tsx
+++ website/src/elements/Footer.tsx
@@ -4,7 +4,7 @@ export default () => {
     return (
         <footer>
             <div className='px-8 py-8 | sm:max-w-[68rem] sm:mx-auto | border-t border-gray-200 dark:border-zinc-700'>
-                <small className='text-gray-400 dark:text-zinc-600'>Made with ☕️ by <a className='hover:underline' href='https://github.com/abiddiscombe'>@abiddiscombe</a></small>
+                <small className='text-gray-400 dark:text-zinc-600'>Made with ☕️ and 🪴 by <a className='hover:underline' href='https://github.com/abiddiscombe'>@abiddiscombe (Archie Biddiscombe)</a></small>
             </div>
         </footer>
     )
